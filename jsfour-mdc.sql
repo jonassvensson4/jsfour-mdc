a72bf511e51a61549a3c4bad937bd01eb32c4104
Jonas Svensson <jonassvensson4@hotmail.com>
2018-08-24 07:51:13 +0200

    Update jsfour-mdc.sql

--- jsfour-mdc.sql
+++ jsfour-mdc.sql
@@ -11,19 +11,19 @@ CREATE TABLE `jsfour_incidents` (
 
 CREATE TABLE `jsfour_cardetails` (
 	`pk` int(11) NOT NULL AUTO_INCREMENT,
-  	`plate` varchar(255) NULL DEFAULT,
- 	`owner` varchar(255) NULL DEFAULT,
+  	`plate` varchar(255) NULL,
+ 	`owner` varchar(255) NULL,
   	`incident` varchar(255) NOT NULL DEFAULT '{}',
-  	`inspected` varchar(255) NULL DEFAULT,
-  	`identifier` varchar(255) NULL DEFAULT,
+  	`inspected` varchar(255) NULL,
+  	`identifier` varchar(255) NULL,
   	PRIMARY KEY (`pk`)
 );
 
 CREATE TABLE `jsfour_logs` (
 	`pk` int(11) NOT NULL AUTO_INCREMENT,
-	`type` varchar(255) NULL DEFAULT,
-	`remover` varchar(255) NULL DEFAULT,
-	`wanted` varchar(255) NULL DEFAULT,
+	`type` varchar(255) NULL,
+	`remover` varchar(255) NULL,
+	`wanted` varchar(255) NULL,
 	PRIMARY KEY (`pk`)
 );
 
