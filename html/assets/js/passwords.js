47ec81b72aebb45ab7e01003cf3db3d71feb1644
Jonas Svensson <jonassvensson4@hotmail.com>
2018-09-09 21:59:37 +0200

    Indepth guide for people with difficulties..

--- html/assets/js/passwords.js
+++ html/assets/js/passwords.js
@@ -1,16 +1,12 @@
 // Password + lastdigits - links the password to the player
 const passwords = {
-  'test123' : 1234,
   'test' : 1234, // test will be the password, 1234 will be the lastdigits of the player
-  't2est' : 1234,
-  'te21s2t' : 1234,
-  'tes2t' : 1234,
-  'te54st' : 1234,
-  't2est' : 1234,
+  'password' : 4321, // password will be the password, 4321 will be the lastdigits of the player
 }
 
-// Latdigits of the players you want to give admin rights too (they can check the logs)
+// Number + Latdigits of the players you want to give admin rights too (they can check the logs)
+// The number starts with 1 and continues.. It's just something the script needs
 const admins = {
-  '1' : 1234,
-  '2' : 5678,
+  '1' : 1234, // 1 is just a number the script needs, 1234 is the lastidigts of the player
+  '2' : 5678, // 2 is just a number the script needs, 5678 is the lastidigts of the player
 }
